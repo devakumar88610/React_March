6660110cdd51cd8160b92072eb35a4c99fe38941
devakumar88610 <devakumar88610@gmail.com>
2023-04-10 14:01:32 +0530

    okie

--- client/src/Components/UserappFunctional/UserApp.jsx
+++ client/src/Components/UserappFunctional/UserApp.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 import UserList from './UserList'
+import UserDetails from './UserDetails'
 
 const UserApp = () => {
 
-    const [state, setState] = useState({ user: {}, selectedUser: {} })
+    const [state, setState] = useState({ user: {} })
+    const [selUser, setSelUser] = useState({ selectedUser: {} })
 
     useEffect(() => {
         Axios.get('https://dummyjson.com/users')
@@ -12,21 +14,27 @@ const UserApp = () => {
                 setState({ user: response.data })
             })
             .catch()
-    })
+    }, [1000])
 
-    const selectedUser = () => {
-
+    const handleSelectedUser = (user) => {
+        setSelUser({ selectedUser: user })
     }
 
     return (
         <>
-            <div className="container mt-5">
-                <pre>{JSON.stringify(state.user)}</pre>
-
+            <div className="container mt-5 ">
+                {/* <pre>{JSON.stringify(state.user)}</pre> */}
                 <div>
                     {
                         Object.keys(state.user).length > 0 ? <>
-                            <UserList users={state.user.users} />
+                            <UserList users={state.user.users} openUsers={handleSelectedUser} />
+                        </> : null
+                    }
+                </div>
+                <div>
+                    {
+                        Object.keys(state.user).length > 0 ? <>
+                            <UserDetails user={selUser.selectedUser} openUsers={handleSelectedUser} />
                         </> : null
                     }
                 </div>
